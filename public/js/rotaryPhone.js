c2122c7cf53fb6620413255b400a7c1c7203da4b
Smithy727 <leanne.smith-44@students.plymouth.ac.uk>
2024-01-06 20:35:41 +0000

    added hang up button

--- public/js/rotaryPhone.js
+++ public/js/rotaryPhone.js
@@ -3,18 +3,17 @@
    The script handles user interactions, animations, and simulates phone call actions. */
 
    var vObj = {
-    // Properties for maintaining the state of the application
-    ani: 0,              // Animation state flag
-    bar: undefined,      // Reference to the bar element for animations
-    conCl: undefined,    // Class list of the container element
-    lines: undefined,    // Collection of line elements for display
-    numArr: [],          // Array to store dialed numbers
-    numCount: 0,         // Count of numbers dialed
-    nums: undefined,     // Collection of number elements
-    title: undefined,    // Reference to the title element for display
-    dialSound: new Audio('./audio/directoryAudio/rotaryDialToneExtended.mp3'),  // Dial sound file
+    ani: 0,
+    bar: undefined,
+    conCl: undefined,
+    lines: undefined,
+    numArr: [],
+    numCount: 0,
+    nums: undefined,
+    title: undefined,
+    dialSound: new Audio('./audio/directoryAudio/rotaryDialToneExtended.mp3'),
+    currentAudio: null,
 
-    // Test audio files for specific number sequences
     mp3Sequences: {
         '1234567890': './audio/directoryAudio/0001.mp3', 
         '2222222222': './audio/directoryAudio/0002.wav',  
@@ -28,22 +27,55 @@
         '1111111111': './audio/directoryAudio/0010.mp3'
     },
 
-    // Method to handle call actions
+    hideEndCallButton: function() {
+        var endCallButton = document.querySelector('.end-call-button');
+        if (endCallButton) {
+            endCallButton.style.display = 'none';
+        }
+    },
+
+    showEndCallButton: function() {
+        var endCallButton = document.querySelector('.end-call-button');
+        if (endCallButton) {
+            endCallButton.style.display = 'block';
+        }
+    },
+
     call: function() {
         if (this.numCount == 10) {
             var dialedNumber = this.numArr.join('');
             if (dialedNumber in this.mp3Sequences) {
-                new Audio(this.mp3Sequences[dialedNumber]).play();
+                if (this.currentAudio && !this.currentAudio.paused) {
+                    this.currentAudio.pause();
+                    this.currentAudio.currentTime = 0;
+                }
+                this.currentAudio = new Audio(this.mp3Sequences[dialedNumber]);
+                this.currentAudio.play();
+                this.title.innerHTML = "End Call";
+                this.currentAudio.onended = () => {
+                    this.title.innerHTML = "Ready to Dial";
+                };
+                this.showEndCallButton();
             } else {
                 this.title.innerHTML = "Please<br>dial 10 digits";
-                setTimeout(function() {
-                    vObj.title.innerHTML = "Press<br>Numbers";
+                setTimeout(() => {
+                    this.title.innerHTML = "Press<br>Numbers";
                 }, 3000);
             }
         }
     },
 
-    // Method to clear the dialed numbers and reset the interface
+    hangUp: function() {
+        if (this.currentAudio) {
+            this.currentAudio.pause();
+            this.currentAudio.currentTime = 0;
+            this.currentAudio = null;
+        }
+        this.clearFun();
+        this.title.innerHTML = "Ready to Dial";
+        this.hideEndCallButton();
+    },
+
     clearFun: function() {
         this.numArr = [];
         this.link.setAttribute('href', '#');
@@ -56,7 +88,6 @@
         }
     },
 
-    // Utility method to remove specific classes from elements
     prune: function(list, tExp) {
         for (let p = 0; p < list.length; p++) {
             if (tExp.test(list[p])) {
@@ -66,7 +97,6 @@
         }
     },
 
-    // Method to handle number button clicks
     numClick: function(num, deg) {
         if (!this.ani && this.numCount < 10) {
             this.dialSound.play();
@@ -75,15 +105,15 @@
             var whole = num * 200 + 500;
             var half = whole / 2;
             this.conCl.add('r' + deg);
-            setTimeout(function() {
-                vObj.bar.add('rotBar');
+            setTimeout(() => {
+                this.bar.add('rotBar');
             }, half);
-            setTimeout(function() {
-                vObj.bar.remove('rotBar');
-                vObj.prune(vObj.conCl, /^r/);
-                vObj.ani = 0;
-                vObj.dialSound.pause();
-                vObj.dialSound.currentTime = 0;
+            setTimeout(() => {
+                this.bar.remove('rotBar');
+                this.prune(this.conCl, /^r/);
+                this.ani = 0;
+                this.dialSound.pause();
+                this.dialSound.currentTime = 0;
             }, whole);
             this.numCount++;
             if (this.numCount == 10) {
@@ -94,7 +124,6 @@
         }
     },
 
-    // Helper methods for updating the display
     updateDisplayForFinalNumber: function(num) {
         this.nums[9].classList.remove('rAFade');
         this.title.innerHTML = "Click<br> to Call";
@@ -110,13 +139,21 @@
 };
 
 function init() {
-    // Function to initialise the application after page load
     vObj.title = document.getElementById('title');
     vObj.conCl = document.getElementById('con1').classList;
     vObj.bar = document.getElementById('bar').classList;
     vObj.nums = document.getElementsByClassName('btnNum2');
     vObj.lines = document.getElementsByClassName('btLine');
     vObj.link = document.getElementById('phoneLink');
+    var dialCenter = document.getElementById('dialCenter');
+    if (dialCenter) {
+        dialCenter.addEventListener('click', function() {
+            vObj.hangUp();
+        });
+    }
+    vObj.hideEndCallButton();
 }
 
 window.onload = init;
+
+  
